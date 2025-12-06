75c1b11c3fae3ce2b69c3e0e8a06e29f1e1b4265
Katy Harper <katyharperofficial@gmail.com>
2025-12-06 23:27:17 +0500

    Update CORS settings for production

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://multiformat-to-pdf.netlify.app/'
+    'https://multiformat-to-pdf.netlify.app'
   ],
   credentials: true
 }));
